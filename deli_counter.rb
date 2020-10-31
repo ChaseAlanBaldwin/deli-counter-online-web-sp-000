f13b87c711ce2c9ac79e51771d6782cf1abdff64
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-31 16:31:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(array)
   line = ""
   if array.length === 0
     puts "The line is currently empty."
-  elsif array.length > 0
-    #array.unshift("The line is currently: ")
+  #elsif array.length > 0
+    array.unshift("The line is currently: ")
     puts array
     end
 
