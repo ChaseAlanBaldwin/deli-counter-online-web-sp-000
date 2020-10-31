cb738b21b9639f832b240c8d6b2b46a44ed58edd
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-31 16:20:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length === 0
+    puts "The line is currently empty."
+  end
+
+  x = 1
+  line = ""
+
+  until x === array.length do
+    line += "#{x}. #{array[x]}"
+    x += 1
+  end
+
+  puts line
+end
+
+def take_a_number(katz_deli, name)
+
+end
