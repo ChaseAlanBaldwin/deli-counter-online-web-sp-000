22662418b2df1fbeb0fe3e288be2f8b8bd913baa
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-31 16:50:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,8 +19,8 @@ end
 
 
 def take_a_number(array, name)
-#  if array.length === 0
-    #array.push(name)
-    #puts "Welcome, #{name}. You are number 1 in line."
-#  end
+ if array.length === 0
+    array.push(name)
+    puts "Welcome, #{name}. You are number 1 in line."
+  end
 end
