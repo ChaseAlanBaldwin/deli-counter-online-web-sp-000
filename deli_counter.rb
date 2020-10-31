29c6f41c582dd7a4739d9e66886f660d4cf76bab
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-31 16:21:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,8 @@
 katz_deli = []
 
 def line(array)
-  if array.length === 0
-    puts "The line is currently empty."
+
   end
-
-  x = 1
-  line = ""
-
-  puts line
 end
 
 def take_a_number(katz_deli, name)
