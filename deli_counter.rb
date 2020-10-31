1b1bb271a6476213bb3d6565adac9f6b32051102
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-31 16:35:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,18 +7,13 @@ def line(array)
   if array.length === 0
     puts "The line is currently empty."
   elsif array.length > 0
-    array.unshift("The line is currently: ")
-    puts array
+     until x == array.length do
+      line += "#{x + 1}. #{array[x]} "
+      x += 1
+     end
+     puts line
   end
-
   end
-
-#    puts line + "#{x}. #{array[x]}"
-# until x == array.length do
-#  line += "#{x + 1}. #{array[x]} "
-#  x += 1
-# end
-# puts line
 
 
 def take_a_number(katz_deli, name)
